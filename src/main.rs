8b427c79457cd6dc2dbf073cf57aecdfaf75f76f
bernabeguzman <bernabeguzman@yahoo.com>
2019-03-17 19:22:51 -0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+// use std::env;
+use std::{i8, i16, i32, i64, u8, u16, u32, u64, isize, usize, f32, f64};
+use std::io::stdin;
+
+// fn main() {
+    // let key = "RUST_DOCKER_NAME";
+
+    // // print Hello World if ENV name not present
+    // match env::var(key) {
+        // Ok(val) => println!("Hello, {}", val),
+        // _ => println!("Hello World"),
+    // }
+// }
+
+fn main() {
+	println!("Hello Wld");
+	let num = 10;
+	let mut age: i32 = 40;
+	println!("u8 Max {}", u8::MAX);
+}
